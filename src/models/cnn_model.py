87556277e5c75dfd9389c16338d6056c7ce6c95e
asif <asahmed8890@gmail.com>
2025-07-17 21:10:11 +0600

    Defined CNN-based deep learning model

--- src/models/cnn_model.py
+++ src/models/cnn_model.py
@@ -21,14 +21,14 @@ class CNNModel:
         self.input_shape = input_shape
         self.model = self._build_model(filters, kernel_size, dense_units, dropout_rate)
 
-    def _build_model(self, filters, kernet_size, dense_units, dropout_rate):
+    def _build_model(self, filters, kernel_size, dense_units, dropout_rate):
         model = Sequential()
-        model.add(Conv1D(filters=filters, kernet_size=kernet_size, activation='relu', input_shape=self.input_shape))
+        model.add(Conv1D(filters=filters, kernel_size=kernel_size, activation='relu', input_shape=self.input_shape))
         model.add(MaxPooling1D(pool_size=2))
         model.add(Flatten())
         model.add(Dense(dense_units, activation='relu'))
         model.add(Dropout(dropout_rate))
-        model.add(Dense(1))
+        model.add(Dense(1))  # Single value regression output
         model.compile(optimizer='adam', loss='mse')
         return model
 
@@ -49,3 +49,29 @@ class CNNModel:
             verbose=1
         )
         return history
+
+    def predict(self, X):
+        """
+        Predict using the trained CNN model.
+        """
+        return self.model.predict(X)
+
+    def evaluate(self, y_true, y_pred):
+        """
+        Compute MAE and RMSE for model performance.
+        """
+        mae = mean_absolute_error(y_true, y_pred)
+        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        return {"MAE": mae, "RMSE": rmse}
+
+    def save_model(self, path):
+        """
+        Save the trained Keras model to disk.
+        """
+        self.model.save(path)
+
+    def load_model(self, path):
+        """
+        Load a trained Keras model from disk.
+        """
+        self.model = load_model(path)
