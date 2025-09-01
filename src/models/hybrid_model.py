2e3d1d7da58fd04b60a6c14a64a2d2a16fbade3f
asif <asahmed8890@gmail.com>
2025-09-01 20:09:19 +0600

    Architected a class-based Hybrid CNN-LSTM model

--- src/models/hybrid_model.py
+++ src/models/hybrid_model.py
@@ -8,7 +8,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 class HybridCNNLSTMModel:
     def __init__(self, input_shape,
                  conv_filters=64, kernel_size=3, pool_size=2,
-                 lstm_units=64, dense_units=32, dropout_rate=0.3):
+                 lstm_units=64, dense_units=32, dropout_rate=0.2):
         """
         Hybrid CNN + LSTM model for stock price prediction.
 
@@ -29,7 +29,7 @@ class HybridCNNLSTMModel:
         model = Sequential()
         # CNN Block
         model.add(Conv1D(filters=conv_filters, kernel_size=kernel_size, activation='relu', input_shape=self.input_shape))
-        model.add(MaxPooling1D(pool_size=pool_size))
+        # model.add(MaxPooling1D(pool_size=pool_size))
         model.add(Dropout(dropout_rate))
 
         # LSTM Block
@@ -37,14 +37,14 @@ class HybridCNNLSTMModel:
         model.add(Dropout(dropout_rate))
 
         # Dense Block
-        model.add(Dense(units=dense_units, activation='relu'))
-        model.add(Dropout(dropout_rate))
+        # model.add(Dense(units=dense_units, activation='relu'))
+        # model.add(Dropout(dropout_rate))
         model.add(Dense(1))  # Regression output (stock price)
 
         model.compile(optimizer='adam', loss='mse')
         return model
 
-    def train(self, X_train, y_train, X_val, y_val, batch_size=32, epochs=100, patience=10):
+    def train(self, X_train, y_train, X_val, y_val, batch_size=32, epochs=100, patience=40):
         """
         Train the Hybrid CNN-LSTM model.
         """
@@ -65,12 +65,30 @@ class HybridCNNLSTMModel:
         """
         return self.model.predict(X)
 
-    def evaluate(self, y_true, y_pred):
+    def evaluate(self, y_true, y_pred, scaler, scaler_shape):
         """
         Evaluate model with MAE and RMSE.
         """
-        mae = mean_absolute_error(y_true, y_pred)
-        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        self.scaler = scaler
+
+        y_test_reshaped = y_true.reshape(-1, 1)
+        y_pred_reshaped = y_pred.reshape(-1, 1)
+
+        y_pred_dummy = np.zeros((len(y_pred), scaler_shape))
+        y_test_dummy = np.zeros((len(y_true), scaler_shape))
+        y_pred_dummy[:, -1] = y_pred_reshaped[:, 0]
+        y_test_dummy[:, -1] = y_test_reshaped[:, 0]
+
+        # Inverse transform only the target
+        y_pred_inv = scaler.inverse_transform(y_pred_dummy)[:, -1]
+        y_test_inv = scaler.inverse_transform(y_test_dummy)[:, -1]
+
+        # Evaluation Metrics
+        mae = mean_absolute_error(y_test_inv, y_pred_inv)
+        rmse = np.sqrt(mean_squared_error(y_test_inv, y_pred_inv))
+
+        # mae = mean_absolute_error(y_true, y_pred)
+        # rmse = np.sqrt(mean_squared_error(y_true, y_pred))
         return {"MAE": mae, "RMSE": rmse}
 
     def save_model(self, path):
