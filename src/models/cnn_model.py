2afbb87358c64f9a01f0b3ea7eaeba12876937d5
asif <asahmed8890@gmail.com>
2025-09-01 20:08:05 +0600

    Defined CNN-based deep learning model

--- src/models/cnn_model.py
+++ src/models/cnn_model.py
@@ -7,7 +7,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 import os
 
 class CNNModel:
-    def __init__(self, input_shape, filters=64, kernel_size=3, dense_units=64, dropout_rate=0.3):
+    def __init__(self, input_shape, filters=64, kernel_size=3, dense_units=50, dropout_rate=0.2):
         """
         Initialize the CNN model.
 
@@ -25,14 +25,14 @@ class CNNModel:
         model = Sequential()
         model.add(Conv1D(filters=filters, kernel_size=kernel_size, activation='relu', input_shape=self.input_shape))
         model.add(MaxPooling1D(pool_size=2))
+        model.add(Dropout(dropout_rate))
         model.add(Flatten())
         model.add(Dense(dense_units, activation='relu'))
-        model.add(Dropout(dropout_rate))
         model.add(Dense(1))  # Single value regression output
-        model.compile(optimizer='adam', loss='mse')
+        model.compile(optimizer='adam', loss='mse', metrics=['mae'])
         return model
 
-    def train(self, X_train, y_train, X_val, y_val, batch_size=32, epochs=100, patience=10):
+    def train(self, X_train, y_train, X_val, y_val, batch_size=16, epochs=100, patience=40):
         """
         Train the CNN model with early stopping.
 
@@ -56,12 +56,30 @@ class CNNModel:
         """
         return self.model.predict(X)
 
-    def evaluate(self, y_true, y_pred):
+    def evaluate(self, y_true, y_pred, scaler, scaler_shape):
         """
         Compute MAE and RMSE for model performance.
         """
-        mae = mean_absolute_error(y_true, y_pred)
-        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        self.scaler = scaler
+
+        y_test_reshaped = y_true.reshape(-1, 1)
+        y_pred_reshaped = y_pred.reshape(-1, 1)
+
+        y_pred_dummy = np.zeros((len(y_pred), scaler_shape))
+        y_test_dummy = np.zeros((len(y_true), scaler_shape))
+        y_pred_dummy[:, -1] = y_pred_reshaped[:, 0]
+        y_test_dummy[:, -1] = y_test_reshaped[:, 0]
+
+        # Inverse transform only the target
+        y_pred_inv = scaler.inverse_transform(y_pred_dummy)[:, -1]
+        y_test_inv = scaler.inverse_transform(y_test_dummy)[:, -1]
+
+        # Evaluation Metrics
+        mae = mean_absolute_error(y_test_inv, y_pred_inv)
+        rmse = np.sqrt(mean_squared_error(y_test_inv, y_pred_inv))
+
+        # mae = mean_absolute_error(y_true, y_pred)
+        # rmse = np.sqrt(mean_squared_error(y_true, y_pred))
         return {"MAE": mae, "RMSE": rmse}
 
     def save_model(self, path):
